d866b363239d08d5ebe97b649e78836e6446b113
RAKESH KUMAR <RAKESH KUMAR@RAKESHKUMAR-PC>
2021-06-09 12:21:19 +0530

    Java Access Modifier

---
+++ Access Modifier/src/helloworld/A.java
@@ -0,0 +1,14 @@
+package helloworld;
+	class A  
+	{  
+	 String msg="Try to access the private variable outside the class";
+	 
+  
+	public static void main(String[] args) {  
+	    A a=new A();  
+	   System.out.println(a.msg);  
+	      
+	}  
+	}  
+
+
